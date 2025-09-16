081362eb097c3071dd9a20cf4a2f29d8ed11260f
harshita01rajput <rajputharshita010@gmail.com>
2025-09-16 11:49:23 +0530

    Create OddEvenNumbers.java

---
+++ ControlFlow/Assignment1/OddEvenNumbers.java
@@ -0,0 +1,22 @@
+// OddEvenNumbers.java
+import java.util.Scanner;
+
+public class OddEvenNumbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a natural number: ");
+        int n = sc.nextInt();
+
+        if (n > 0) {
+            for (int i = 1; i <= n; i++) {
+                if (i % 2 == 0) {
+                    System.out.println(i + " is even");
+                } else {
+                    System.out.println(i + " is odd");
+                }
+            }
+        } else {
+            System.out.println("Invalid input. Enter a natural number.");
+        }
+    }
+}
